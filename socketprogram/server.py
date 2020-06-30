c5cef1177dd03b70802be8dc3d423f7b4837e0a5
Bishal <bishal1banerjee@gmail.com>
2020-06-30 14:34:23 +0530

    First Commit

---
+++ socketprogram/server.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+# SERVER = "192.168.43.122"
+SERVER = socket.gethostbyname(socket.gethostname())  # Get IPv4 address automatically
+ADDR = (SERVER, PORT)  # BINDING SOCKET
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION]: {addr} connected.")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+
+        print(f"[{addr}] {msg}")
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING]: Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print("[ACTIVE CONNECTIONS]: ", threading.activeCount() - 1)
+
+
+print("[STARTING]: Server is starting....")
+start()
